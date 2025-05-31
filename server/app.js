85b94c520988899af04a13b7834d44464837568b
Victor Vikhal <victor.vikhal@gmail.com>
2025-05-31 16:40:50 +0200

    routes for student

--- server/app.js
+++ server/app.js
@@ -5,15 +5,14 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
 const mongoose = require("mongoose");
-const Cohort = require("./server/models/cohort.model");
+const Cohort = require("./models/cohort.model.js");
 
-const Student = require("./server/models/student");
+const Students = require("./models/student.model.js");
 
 const PORT = 5005;
 
 // STATIC DATA - (you'll eventually remove this once DB is fully integrated)
-const students = require("./students.json");
-const cohorts = require("./cohorts.json");
+
 
 // INITIALIZE EXPRESS APP
 const app = express();
@@ -31,9 +30,51 @@ app.get("/docs", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "docs.html"));
 });
 
+//calling all students
 app.get("/api/students", (req, res) => {
-  res.json(students); // Later: replace with MongoDB find()
+
+  Students.find()
+  .then(response => {
+    res.json(response)
+  })
+  .catch(error => res.status(500).json(error))
+   // Later: replace with MongoDB find()
 });
+
+// calling students with specified cohort
+app.get("/api/students/cohort/:cohortId", (req, res, next) =>{
+  const {cohortId} = req.params
+//encuentra los estudiantes que tengan como cohort al cohortId.
+  Students.find(({ cohort: cohortId })).populate('cohort')  
+  .then(response =>{
+    res.json(response)
+  })
+  .catch(error => res.status(500).json(error))
+})
+
+
+// calling specified student
+app.get("/api/students/:studentId", (req, res, next) =>{
+  const {studentId} = req.params
+  Students.findById(studentId)
+  .then(response =>{
+    res.json(response)
+  })
+  .catch(error => res.status(500).json(error))
+})
+
+//creating new student
+app.post("/api/students", (req, res, next) =>{
+    Students.create()
+})
+//updating specified student
+app.put("/api/students/:studentId", (req, res, next) =>{
+  const {studentId} = req.params
+  const updatedStudent = req.body
+  Students.findByIdAndUpdate(studentId, updatedStudent, {new: true})
+})
+
+
 
 app.get("/api/cohorts", (req, res) => {
   res.json(cohorts); // Later: replace with MongoDB find()
@@ -54,7 +95,7 @@ mongoose
   })
 
   .then(() => {
-    return students.find()
+    return Students.find()
   })
 
   .then((response) => {
